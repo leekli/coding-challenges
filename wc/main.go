3e9cbeec77e5e5315f3bd54baf926317f524c1f2
leekli <li.kirkham@live.com>
2024-07-28 16:15:23 +0100

    adds -l flag

--- wc/main.go
+++ wc/main.go
@@ -16,10 +16,20 @@ func main() {
 	filePathGiven := argsGiven[len(argsGiven)-1]
 
 	if utils.HasArgs(argsGiven) {
+		// Count Bytes
 		if argsGiven[0] == "-c" {
 			totalByteCount := countBytes(filePathGiven)
 
 			formattedResult := fmt.Sprintf("  %d %s", totalByteCount, filePathGiven)
+
+			fmt.Println(formattedResult)
+		}
+
+		// Count Lines
+		if argsGiven[0] == "-l" {
+			totalLineCount := countLines(filePathGiven)
+
+			formattedResult := fmt.Sprintf("  %d %s", totalLineCount, filePathGiven)
 
 			fmt.Println(formattedResult)
 		}
@@ -32,4 +42,18 @@ func countBytes(filePath string) int {
 	totalBytes := len(fileContents)
 
 	return totalBytes
+}
+
+func countLines(filePath string) int {
+	fileContents := utils.ReadFile(filePath)
+
+	lineCount := 0
+
+	for _, byte := range fileContents {
+		if byte == '\n' {
+			lineCount++
+		}
+	}
+
+	return lineCount
 }
